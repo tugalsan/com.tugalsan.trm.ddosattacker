7445d77e65c8f391e1919034b9839a247971fd71
tugalsan karabacak <tugalsan@gmail.com>
2023-01-13 07:00:05 +0300

    .

---
+++ run.bat
@@ -0,0 +1,1 @@
+java --enable-preview --add-modules jdk.incubator.concurrent -jar target/com.tugalsan.trm.ddosattacker-1.0-SNAPSHOT-jar-with-dependencies.jar
--- src/main/java/com/tugalsan/trm/ddosattacker/Main.java
+++ src/main/java/com/tugalsan/trm/ddosattacker/Main.java
@@ -1,18 +1,22 @@
 package com.tugalsan.trm.ddosattacker;
 
-import com.tugalsan.api.thread.server.TS_ThreadRun;
-import com.tugalsan.api.url.client.TGS_Url;
-import com.tugalsan.api.url.server.TS_UrlDDosUtils;
+import com.tugalsan.api.thread.server.*;
+import com.tugalsan.api.url.client.*;
+import com.tugalsan.api.url.server.*;
 import java.util.stream.*;
 
 public class Main {
+    //cd C:\me\codes\com.tugalsan\trm\com.tugalsan.trm.ddosattacker
+    //java --enable-preview --add-modules jdk.incubator.concurrent -jar target/com.tugalsan.trm.ddosattacker-1.0-SNAPSHOT-jar-with-dependencies.jar
 
     public static void main(String... args) {
-        var url = TGS_Url.of("https://tugalsan.com:8443");
-        IntStream.range(0, 2000).forEach(i -> TS_ThreadRun.now(() -> {
+        var url = TGS_Url.of("https://localhost:8081/hello1");
+        IntStream.range(0, 2000000).forEach(i -> TS_ThreadRun.now(() -> {
             while (true) {
                 TS_UrlDDosUtils.attack(url);
+                System.out.println(".");
             }
         }));
+        TS_ThreadWait.minutes(null, 1);
     }
 }
