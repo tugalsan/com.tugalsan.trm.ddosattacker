f42b30d9fe97fa00cc6c751d5b44ba74b603a106
tugalsan karabacak <tugalsan@gmail.com>
2023-04-17 16:29:21 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,5 @@ module com.tugalsan.trm.ddosattacker {
     requires gwt.user;
     requires com.tugalsan.api.thread;
     requires com.tugalsan.api.url;
-    requires com.tugalsan.api.executable;
+    requires com.tugalsan.api.runnable;
 }
