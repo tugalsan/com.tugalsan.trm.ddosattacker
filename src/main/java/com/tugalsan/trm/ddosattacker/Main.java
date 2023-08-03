90e660104865155bba7b36f189529a5d4e5fc803
tugalsan karabacak <tugalsan@gmail.com>
2023-08-03 15:06:06 +0300

    .

--- src/main/java/com/tugalsan/trm/ddosattacker/Main.java
+++ src/main/java/com/tugalsan/trm/ddosattacker/Main.java
@@ -1,7 +1,9 @@
 package com.tugalsan.trm.ddosattacker;
 
+import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.thread.server.async.TS_ThreadAsync;
 import com.tugalsan.api.thread.server.TS_ThreadWait;
+import com.tugalsan.api.thread.server.safe.TS_ThreadSafeTrigger;
 import com.tugalsan.api.url.client.*;
 import com.tugalsan.api.url.server.*;
 import java.util.stream.*;
@@ -10,14 +12,17 @@ public class Main {
     //cd C:\me\codes\com.tugalsan\trm\com.tugalsan.trm.ddosattacker
     //java --enable-preview --add-modules jdk.incubator.concurrent -jar target/com.tugalsan.trm.ddosattacker-1.0-SNAPSHOT-jar-with-dependencies.jar
 
+    final private static TS_Log d = TS_Log.of(Main.class);
+    final private static TS_ThreadSafeTrigger killTrigger = TS_ThreadSafeTrigger.of();
+
     public static void main(String... args) {
         var url = TGS_Url.of("https://localhost:8081/hello1");
-        IntStream.range(0, 2000000).forEach(i -> TS_ThreadAsync.now(() -> {
+        IntStream.range(0, 2000000).forEach(i -> TS_ThreadAsync.now(killTrigger, kt -> {
             while (true) {
                 TS_UrlDDosUtils.attack(url);
                 System.out.println(".");
             }
         }));
-        TS_ThreadWait.minutes(null, 1);
+        TS_ThreadWait.minutes(d.className, killTrigger, 1);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.tugalsan.trm.ddosattacker {
     requires gwt.user;
     requires com.tugalsan.api.thread;
+    requires com.tugalsan.api.log;
     requires com.tugalsan.api.url;
     requires com.tugalsan.api.runnable;
 }
