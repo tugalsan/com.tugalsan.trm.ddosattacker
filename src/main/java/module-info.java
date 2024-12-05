75107b331fe17bff1488802047dd683bf06a26e5
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-12-05 13:34:50 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,5 @@ module com.tugalsan.trm.ddosattacker {
     requires com.tugalsan.api.thread;
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.url;
-    requires com.tugalsan.api.runnable;
+    requires com.tugalsan.api.function;
 }
